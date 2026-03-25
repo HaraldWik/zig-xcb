4f02fcebf7180c1977a729e609230fb24d7079a1
HaraldWik <wikhallharald@gmail.com>
2026-03-25 21:52:13 +0100

    zig module

---
+++ example/c/build.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const xcb = b.dependency("xcb", .{ .target = target, .optimize = optimize });
+
+    const exe = b.addExecutable(.{
+        .name = "simple",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
+    });
+    exe.root_module.addCSourceFile(.{ .file = b.path("src/main.c") });
+    exe.root_module.linkLibrary(xcb.artifact("xcb"));
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| run_cmd.addArgs(args);
+}
